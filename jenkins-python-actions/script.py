aa821b24cbb81333a67a63e5b60d70e27cd5aef7
Osama Ahmed Tahir <osamaahmedtahir17@gmail.com>
2025-04-27 16:15:02 +0200

    Small space removed

--- jenkins-python-actions/script.py
+++ jenkins-python-actions/script.py
@@ -1,4 +1,4 @@
 from datetime import datetime
 
 now = datetime.now()
-print("This is the time now ", now.strftime("%Y-%m-%d %H:%M:%S"))
+print("This is the time now", now.strftime("%Y-%m-%d %H:%M:%S"))
