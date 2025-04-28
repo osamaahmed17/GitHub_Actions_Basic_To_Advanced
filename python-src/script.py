ea50fb9a107cc75f73002b9578a30afa5f2ebb96
Osama Ahmed Tahir <osamaahmedtahir17@gmail.com>
2025-04-29 00:55:28 +0200

    updated time

--- python-src/script.py
+++ python-src/script.py
@@ -12,4 +12,4 @@ parser.add_argument('--input2', type=str, required=True, help='Second input')
 args = parser.parse_args()
     
 print("time now" +now.strftime("%Y-%m-%d %H:%M:%S")+ args.input1)
-print("Itime now" +now.strftime("%Y-%m-%d %H:%M:%S")+args.input2)
+print("time now" +now.strftime("%Y-%m-%d %H:%M:%S")+args.input2)
